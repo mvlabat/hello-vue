700d3a2125748cbb8ebc8393726502d426720cd8
mvlabat <mvlabat@gmail.com>
2021-03-08 20:14:43 +0200

    Add 404.html fallback page

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let defaultArgs;
+
 module.exports = {
-    publicPath: '/hello-vue',
-}
+  publicPath: '/hello-vue',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = __dirname + '/public/index.html';
+        defaultArgs = args;
+        return args;
+      });
+    config
+      .plugin('html-404')
+      .use(HtmlWebpackPlugin, [{
+        templateParameters: defaultArgs[0].templateParameters,
+        template: __dirname + '/public/404.html',
+        filename: '404.html',
+      }]);
+    config
+      .plugin('copy')
+      .tap(args => {
+        args[0][0].ignore.push({
+          glob: '404.html',
+          matchBase: false
+        });
+        return args;
+      });
+  },
+};
